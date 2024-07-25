37fa174a6385e544651da1ade33cbac595c9f834
Paolo <paoloquintiliani91@gmail.com>
2024-07-24 21:09:53 -0400

    dofor

---
+++ assets/whilefor.js
@@ -0,0 +1,19 @@
+/* var contador =0;
+while (contador <10);{
+console.log('Estoy en la vuelta N°10' + contador);
+contador++; /* podria usar contador+contador+1 */
+/* } */
+/* no ejecutar el while */
+
+/* for( var i = 1; i<=10; i++){
+console.log('Vuelta n°' + i);
+} */
+
+
+/* do while */
+var contador = 0; /* si ponemos 11, la condicion no se cumplira */
+do {
+    console.log('Vuelta n°' + contador);
+    contador++;
+}
+while (contador <= 10)
